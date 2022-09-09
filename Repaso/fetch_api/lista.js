11f82fef57400108cb1d098cbf47391b10381361
Matias Fuentes <matiasfuentes2909@gmail.com>
2022-09-09 16:15:55 -0300

    Resolucion de error en lista

--- Repaso/fetch_api/lista.js
+++ Repaso/fetch_api/lista.js
@@ -3,12 +3,12 @@ async function obtener_usuarios(){
     let usuarios = await fetch('https://jsonplaceholder.typicode.com/users');
 
     let usuarios_definitivo = await usuarios.json();
-    
+
 
     let items = [];
     usuarios_definitivo.forEach(element => {
        
-        let item = `<li class="list-group-item">${element,username}<li>`;
+        let item = `<li class="list-group-item">${element.username}</li>`;
 
         items.push(item);
     });
