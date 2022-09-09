b7c4267e5de86095fc62c78400c25810c2dcfa80
Matias Fuentes <matiasfuentes2909@gmail.com>
2022-09-09 14:37:41 -0300

    Subo cambio de prueba

--- Repaso/fetch_api/lista.js
+++ Repaso/fetch_api/lista.js
@@ -3,6 +3,7 @@ async function obtener_usuarios(){
     let usuarios = await fetch('https://jsonplaceholder.typicode.com/users');
 
     let usuarios_definitivo = await usuarios.json();
+    
 
     let items = [];
     usuarios_definitivo.forEach(element => {
